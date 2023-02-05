6d9779bc96cab6ce96034657286b16de3d09422c
yoyobara <yotambarak6@gmail.com>
2023-02-05 21:04:59 +0200

    script for neovim

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
+ROOT=$(dirname $(realpath $0))
 
 # shell init
-ln -sf ${PWD}/shell_init ${HOME}/
+ln -sf ${ROOT}/shell_init ${HOME}/
 
 # bashrc
-ln -sf ${PWD}/.bashrc ${HOME}/
+ln -sf ${ROOT}/.bashrc ${HOME}/
+
+# neovim
+ln -sf ${ROOT}/.config/nvim ${HOME}/.config/nvim
