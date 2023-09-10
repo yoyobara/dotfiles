e86a989d869ed9d4f6f5f2dd5b9a91227fb2427e
yoyobara <yotambarak6@gmail.com>
2023-09-10 08:29:34 +0300

    okok

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 ROOT=$(dirname $(realpath $0))
+echo "root dir is ${ROOT}"
 
 # shell init
 ln -sf ${ROOT}/shell_init ${HOME}/
-echo "shell init copied"
+echo "shell init linked"
 
 # bashrc
 ln -sf ${ROOT}/.bashrc ${HOME}/
-echo ".bashrc copied"
+echo ".bashrc linked"
