7c30e30d62ec18aaed2cc8106cda30fc59055160
yoyobara <yotambarak6@gmail.com>
2023-09-10 08:46:31 +0300

    vim added

---
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,20 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- if you just want default config for the servers then put them in a table
+local servers = { "html", "cssls", "tsserver", "clangd", "rust_analyzer" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+require'lspconfig'.pyright.setup{}
+require'lspconfig'.asm_lsp.setup{}
+
+-- 
+-- lspconfig.pyright.setup { blabla}
