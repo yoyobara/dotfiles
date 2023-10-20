10bb70cf6af36da17df1d811607078c5b19722b6
yoyobara <yotambarak6@gmail.com>
2023-10-20 21:27:15 +0300

    tabs and line numbers

--- dotfiles/nvchad/custom/chadrc.lua
+++ dotfiles/nvchad/custom/chadrc.lua
@@ -17,4 +17,13 @@ M.plugins = "custom.plugins"
 -- check core.mappings for table structure
 M.mappings = require "custom.mappings"
 
+-- Tab options. 4 spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Enable line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
 return M
