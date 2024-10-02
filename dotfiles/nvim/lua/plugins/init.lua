aa437ab9a2694386500e7dd01229201c9aeb1b05
yoyobara <yotambarak6@gmail.com>
2024-10-02 14:55:11 +0300

    added and configured maven.nvim

--- dotfiles/nvim/lua/plugins/init.lua
+++ dotfiles/nvim/lua/plugins/init.lua
@@ -50,4 +50,14 @@ return {
             save_path = "~/Pictures/p.png"
         }
     },
+    {
+        "eatgrass/maven.nvim",
+        cmd = { "Maven", "MavenExec" },
+        dependencies = "nvim-lua/plenary.nvim",
+        config = function()
+            require('maven').setup({
+                executable="./mvnw"
+            })
+        end
+    },
 }
